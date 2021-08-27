c16b851f40ebfc261e615798ced7bf2b593acb3b
Bryan Dallas <Bryan.M.Dallas@gmail.com>
2021-08-27 17:27:39 -0500

    Initial structure set up. Test functionality not implemented, but code compiles and runs.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "example.h"
+
+#include <memory>
+
+int main(int argc, char **argv)
+{
+    // Use a shared_ptr to ensure that it is cleaned up when the
+    // program exits.
+    auto example_ptr = std::make_shared<example::Example>();
+    return 0;
+}
