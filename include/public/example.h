c3fea2391484dec2c6c7c7c0e8cfac169a06d56f
Bryan Dallas <Bryan.M.Dallas@gmail.com>
2021-09-03 10:07:12 -0500

    Updates to include formatting.

--- include/public/example.h
+++ include/public/example.h
@@ -1,14 +1,17 @@
 // Most modern compilers support this now.
 #pragma once
 
-namespace example
+// This include is minimum C++17 compatible
+#include <filesystem>
+
+namespace linux
 {
 
 /**
  * Currently this class has no functionality, but is used as example
  * code.
  */
-class Example
+class ADriver
 {
 public:
     /** 
@@ -16,14 +19,34 @@ public:
      * it does not get automatically created based on the compilers
      * interpretation of casting code.
      */
-    explicit Example() = default;
+    explicit ADriver() = default;
 
     /**
      * Create the default constructor, ensure that it is virtual.
      */
-    virtual ~Example() = default;
+    virtual ~ADriver() = default;
 
 private:
+};
+
+class ACharacterDriver : public ADriver
+{
+public:
+    /**
+     * The default constructor takes a filesystem path that points to the
+     * linux character device which the driver will use.
+     */
+    explicit ACharacterDriver(const std::filesystem::path&);
+    
+    /**
+     * This is the destructor, uses the C++11 compatible override keyword
+     * instead of virtual.
+     */
+    ~ACharacterDriver() override = default;
+
+private:
+    //! This holds the filesystem path to the device (e.g. /dev/tty0)
+    std::filesystem::path _device_path;
 };
 
 } // namespace example
--- src/example.cpp
+++ src/example.cpp
@@ -1,1 +1,13 @@
 #include "example.h"
+
+namespace linux
+{
+
+ACharacterDriver::ACharacterDriver(const std::filesystem::path& path) :
+    _device_path(path)
+{
+
+    
+}
+
+} // namespace linux
