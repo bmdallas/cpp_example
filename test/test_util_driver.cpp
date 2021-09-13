fdb8cb5ec7302392b2279d933ea456a9c60c4e40
Bryan Dallas <Bryan.M.Dallas@gmail.com>
2021-09-12 19:20:44 -0500

    Fixed formatting

--- test/test_util_driver.cpp
+++ test/test_util_driver.cpp
@@ -1,8 +1,8 @@
 #include "gtest/gtest.h"
 
-TEST(ExampleTest, BasicTest) { 
-    // At some point this would actually test the functionality of the library
-    EXPECT_EQ(1, 1); 
+TEST(ExampleTest, BasicTest) {
+  // At some point this would actually test the functionality of the library
+  EXPECT_EQ(1, 1);
 }
 
 int main(int argc, char **argv) {
